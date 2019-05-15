959cce3f0677e5274c8247a999bfd389399af3b1
eldarj <eldar.jah@gmail.com>
2019-05-15 20:56:57 +0200

    Adding custom model binding for updating and deleting directories.

---
+++ Api/Filters/Binding/CustomModelBinder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Filters.Binding
+{
+    public class CustomModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var s = bindingContext;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Api/Filters/Binding/CustomModelBinderProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Filters.Binding
+{
+    public class CustomModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            var c = context;
+
+            return new CustomModelBinder();
+        }
+    }
+}
