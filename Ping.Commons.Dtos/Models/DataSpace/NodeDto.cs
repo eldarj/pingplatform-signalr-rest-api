7ad487d9ec87c4e5964dd5b8490c3f06575b1157
eldarj <eldar.jah@gmail.com>
2019-05-15 21:28:53 +0200

    Add Url field to Node Dto model.

--- Ping.Commons.Dtos/Models/DataSpace/NodeDto.cs
+++ Ping.Commons.Dtos/Models/DataSpace/NodeDto.cs
@@ -7,6 +7,7 @@ namespace Ping.Commons.Dtos.Models.DataSpace
     {
         public string Name { get; set; }
         public string Path { get; set; }
+        public string Url { get; set; }
         public DateTime CreationTime { get; set; }
         public DateTime LastModifiedTime { get; set; }
         public string OwnerFirstname { get; set; }
