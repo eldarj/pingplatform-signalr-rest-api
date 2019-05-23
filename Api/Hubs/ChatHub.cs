1b65d47cb46b78bcd1a921aec863afa24c8eb910
eldarj <eldar.jah@gmail.com>
2019-05-23 18:28:19 +0200

    Add signalr ChatHub.

---
+++ Api/Hubs/ChatHub.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Api.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+
+        #region ConnectDisconnect
+        public override async Task OnConnectedAsync()
+        {
+            QueryString queryString = Context.GetHttpContext().Request.QueryString;
+            NameValueCollection qs = HttpUtility.ParseQueryString(queryString.ToString());
+            String groupName = qs.Get("groupName");
+            if (groupName != null)
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            }
+
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            QueryString queryString = Context.GetHttpContext().Request.QueryString;
+            NameValueCollection qs = HttpUtility.ParseQueryString(queryString.ToString());
+            String groupName = qs.Get("groupName");
+
+            if (groupName != null)
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            }
+
+            await base.OnDisconnectedAsync(exception);
+        }
+        #endregion  
+    }
+}
