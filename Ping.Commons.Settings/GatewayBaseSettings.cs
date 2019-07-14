436eca8225842021c2dd586e26984f108de77a1b
eldarj <eldar.jah@gmail.com>
2019-07-14 14:50:23 +0200

    Adding new Ping.Commons.SignalR project (BaseHubClientService class) & moving GatewayBaseSettings class into Ping.Commons.Settings proj

---
+++ Ping.Commons.Settings/GatewayBaseSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ping.Commons.Settings
+{
+    public class GatewayBaseSettings
+    {
+        public string Scheme { get; set; }
+        public string Url { get; set; }
+        public string Port { get; set; }
+    }
+}
---
+++ Ping.Commons.SignalR/Base/BaseHubClientService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Options;
+using Ping.Commons.Settings;
+
+
+namespace Ping.Commons.SignalR.Base
+{
+    public class BaseHubClientService
+    {
+        protected readonly HubConnection hubConnection;
+
+        protected readonly SecuritySettings securitySettings;
+
+        public BaseHubClientService(IOptions<GatewayBaseSettings> gatewayBaseOptions, IOptions<SecuritySettings> securityOptions, string hubEndpoint)
+        {
+            this.securitySettings = securityOptions.Value;
+            GatewayBaseSettings gatewayBaseSettings = gatewayBaseOptions.Value;
+
+            string connectionBaseUrl = gatewayBaseSettings.Scheme + "://" + gatewayBaseSettings.Url + ":" + gatewayBaseSettings.Port;
+
+            string accessToken = JWTokenHandler.GenerateToken(securitySettings.ClientIdentifier, securitySettings.Secret);
+
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl(connectionBaseUrl + "/" + hubEndpoint,
+                    options => options.AccessTokenProvider = () => Task.FromResult<string>(accessToken))
+                .Build();
+        }
+    }
+}
